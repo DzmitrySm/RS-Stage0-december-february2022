08dccef4665fdb6e99a966357016bb5103b7d2b9
DzmitrySm <magicjoker@yandex.ru>
2022-01-30 01:11:24 +0300

    feat: add possibility to change seasons by click on portfolio page

--- portfolio/index.js
+++ portfolio/index.js
@@ -15,10 +15,33 @@ console.log('Соответствие макету:42\nПоведение при
     closeMenuByLink.forEach((element) => element.addEventListener('click', () => {
         navMenu.classList.remove('nav-style-active');
     }))
-        
+
+    const portfolioButton = document.querySelector('.button-transparent')
+    const portfolioImage = document.querySelectorAll('.portfolio-image')
+    portfolioButton.addEventListener('click', () => {
+        portfolioImage.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`);
+    });
+
+    const portfolioButtonSpring = document.querySelector('.button-spring')
+    portfolioButtonSpring.addEventListener('click', () => {
+        portfolioImage.forEach((img, index) => img.src = `./assets/img/spring/${index + 1}.jpg`);
+    
+    });
+
+    const portfolioButtonSummer = document.querySelector('.button-summer')
+    portfolioButtonSummer.addEventListener('click', () => {
+        portfolioImage.forEach((img, index) => img.src = `./assets/img/summer/${index + 1}.jpg`);
+    });
+
+    const portfolioButtonAutumn = document.querySelector('.button-autumn')
+    portfolioButtonAutumn.addEventListener('click', () => {
+        portfolioImage.forEach((img, index) => img.src = `./assets/img/autumn/${index + 1}.jpg`);
+    })
     
         
 }());
+
+
 
 
 
