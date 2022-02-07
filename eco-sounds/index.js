6638ec2b4f26290555dd2f13882ee71c30ff6986
DzmitrySm <magicjoker@yandex.ru>
2022-02-08 01:05:08 +0300

    feat: add possibility to change images by click

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -13,17 +13,53 @@ function playAudio() {
     } else if(isPlay) {
         audio.pause()
         isPlay = false
-    } 
-    
+    }
        
 }
 buttonSound.addEventListener('click', playAudio)
 
  const button = document.querySelector('button');
  function toggleBtn() {
-    button.classList.toggle('pause');
+    button.classList.toggle('pause')
  }
  button.addEventListener('click', toggleBtn)
  
+ 
+const navButton = document.querySelector('.solovei')
+const playerImage = document.querySelector('.image-audio-player')
+navButton.addEventListener('click', () => {
+playAudio()
+playerImage.src = "./assets/img/solovey.jpg"
+});
+navButton.addEventListener('click', toggleBtn)
 
+const navButton1 = document.querySelector('.drozd')
+navButton1.addEventListener('click', () => {
+playerImage.src = "./assets/img/drozd.jpg"
+});
+navButton1.addEventListener('click', toggleBtn)
 
+const navButton2 = document.querySelector('.zarynka')
+navButton2.addEventListener('click', () => {
+playerImage.src = "./assets/img/zarynka.jpg"
+});
+navButton2.addEventListener('click', toggleBtn)
+
+const navButton3 = document.querySelector('.javoronok')
+navButton3.addEventListener('click', () => { 
+playerImage.src = "./assets/img/javoronok.jpg"
+});
+navButton3.addEventListener('click', toggleBtn)
+
+const navButton4 = document.querySelector('.slavka')
+navButton4.addEventListener('click', () => { 
+playerImage.src = "./assets/img/slavka.jpg"
+});
+navButton4.addEventListener('click', toggleBtn)
+
+const buttonLogo = document.querySelector('.logo')
+buttonLogo.addEventListener('click', () => {
+playAudio()
+playerImage.src = "./assets/img/forest.jpg"
+});
+buttonLogo.addEventListener('click', toggleBtn)
