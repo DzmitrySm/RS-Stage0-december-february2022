d4025e742ac9add3e5b9b02f67c461f086b9eeb2
DzmitrySm <magicjoker@yandex.ru>
2022-02-12 22:47:32 +0300

    feat: add random jokes by click, and possibility to change background-color by click, add changes to CSS

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,1 +1,42 @@
 console.log('Hello')
+
+
+
+
+
+//async function getQuotes () {
+    //const quotes = 'https://api.icndb.com/jokes/random';
+   // const res = await fetch(quotes);
+    //const data = await res.json()
+   // console.log(data)
+    
+//}
+//let data = getQuotes ()
+//console.log(data)
+function getData() {
+    fetch('https://api.icndb.com/jokes/random')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+       textCont (data) 
+
+      });
+  }
+  getData();
+
+
+let data = getData();
+let paragraph = document.querySelector('.quotes')
+let button = document.querySelector('.button-joke')
+let main = document.querySelector('.main')
+function textCont (data) {
+    paragraph.textContent = data.value.joke
+     
+}
+button.addEventListener('click', () => {
+    main.classList.toggle('active')
+    getData()
+});
+
+
+
