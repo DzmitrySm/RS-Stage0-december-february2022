bbd21c6275b250acf72afad439f5c4a4bf410fb2
DzmitrySm <magicjoker@yandex.ru>
2022-02-21 22:03:25 +0300

    feat: add changes for design of the project and finish it

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -8,7 +8,7 @@ const voiceOfWin = document.querySelector('audio')
 
 zone.addEventListener('click', e => {
 if(e.target.className === 'tic-tac-item') {
-    move % 2 === 0 ? e.target.innerHTML = 'x' : e.target.innerHTML = '0'
+    move % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = '0'
     move++
     check ()
 }
@@ -29,14 +29,13 @@ const check = () => {
 
 
     for (let i = 0; i < arr.length; i++) {
-    if (boxes[arr[i][0]].innerHTML == 'x' && boxes[arr[i][1]].innerHTML == 'x' && boxes[arr[i][2]].innerHTML == 'x') {
+    if (boxes[arr[i][0]].innerHTML == 'X' && boxes[arr[i][1]].innerHTML == 'X' && boxes[arr[i][2]].innerHTML == 'X') {
         result = 'Crosses'
         whoWins(result)
     }
     else if (boxes[arr[i][0]].innerHTML == '0' && boxes[arr[i][1]].innerHTML == '0' && boxes[arr[i][2]].innerHTML == '0') {
        result = 'Zeros'
        whoWins(result)
-    
     }
     }
     
@@ -49,9 +48,11 @@ const check = () => {
 const whoWins = winner => {
     if (result === 'Zeros' || result === 'Crosses') {
         content.innerHTML = `${winner} won! It took ${move} moves`
+        localStorage.setItem('count of moves', move)
         playAudio()  
     } else {
         content.innerHTML = `${winner}, try again! It took ${move} moves`
+        localStorage.setItem('count of moves', move)
         playAudio() 
     }
     modalWindow.classList.add('active')
@@ -67,3 +68,7 @@ function playAudio() {
     voiceOfWin.currentTime = 0;
     voiceOfWin.play()
 }
+
+
+
+
